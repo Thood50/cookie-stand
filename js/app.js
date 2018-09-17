3aee2b0ec7ae0e87b59e3c757378cbf24f123680
tyler <tyler.r.hood@gmail.com>
2018-09-17 12:49:37 -0700

    added objects to app.js

---
+++ js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var globalTime = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
+
+var firstAndPike = {
+  minimumCustomers: 0,
+  maximumCustomers: 0,
+  averageCookies: 0
+};
+
+var seatacAirport = {
+  minimumCustomers: 0,
+  maximumCustomers: 0,
+  averageCookies: 0
+};
+
+var seattleCenter = {
+  minimumCustomers: 0,
+  maximumCustomers: 0,
+  averageCookies: 0
+};
+
+var capitolHill = {
+  minimumCustomers: 0,
+  maximumCustomers: 0,
+  averageCookies: 0
+};
+
+var alki = {
+  minimumCustomers: 0,
+  maximumCustomers: 0,
+  averageCookies: 0
+};
