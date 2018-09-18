84d6dbb2f3b0aba1a1218e0ce2b5a1463767425b
tyler <tyler.r.hood@gmail.com>
2018-09-17 18:32:12 -0700

    added some variables and functions to calculate the values of customer to cookie ratio per location

--- js/app.js
+++ js/app.js
@@ -1,54 +1,142 @@
 'use strict';
 
+var locationArray = [];
 var globalTime = [6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8];
-var locationArray = [firstAndPike, seatacAirport, seattleCenter, capitolHill, alki]
+
+var customerPike = [];
+var customerAirport = [];
+var customerCenter = [];
+var customerHill = [];
+var customerAlki = [];
+
+var pikeSum = 0;
+var airportSum = 0;
+var centerSum = 0;
+var hillSum = 0;
+var alkiSum = 0;
+
+var pikeCookies = 6.3;
+var airportCookies = 1.2;
+var centerCookies = 3.7;
+var hillCookies = 2.3;
+var alkiCookies = 4.6;
+
+var pikeTotal = 0;
+var airportTotal = 0;
+var centerTotal = 0;
+var hillTotal = 0;
+var alkiTotal = 0;
 
 var firstAndPike = {
-  minimumCustomers: 0,
-  maximumCustomers: 0,
-  averageCookies: 0,
-  randomNum: distributeRandomNumber
+  minimumCustomers: 23,
+  maximumCustomers: 65,
+  averageCookies: 6.3,
+  randomNum: function() {
+    console.log('1st and Pike')
+    for (var i = 0; i < globalTime.length; i++) {
+      customerPike.push(
+        getRandomNumber(23, 65)
+      )
+      console.log(globalTime[i] + ' - ' + customerPike[i]);
+    }
+  }
+
 };
 
+
+
 var seatacAirport = {
-  minimumCustomers: 0,
-  maximumCustomers: 0,
-  averageCookies: 0,
-  randomNum: distributeRandomNumber
+  minimumCustomers: 3,
+  maximumCustomers: 24,
+  averageCookies: 1.2,
+  randomNum: function() {
+    console.log('SeaTac Airport')
+    for (var i = 0; i < globalTime.length; i++) {
+      customerAirport.push(
+        getRandomNumber(3, 24)
+      )
+      console.log(globalTime[i] + ' - ' + customerAirport[i]);
+    }
+  }
 };
 
 var seattleCenter = {
-  minimumCustomers: 0,
-  maximumCustomers: 0,
-  averageCookies: 0,
-  randomNum: distributeRandomNumber
+  minimumCustomers: 11,
+  maximumCustomers: 38,
+  averageCookies: 3.7,
+  randomNum: function() {
+    console.log('Seattle Center')
+    for (var i = 0; i < globalTime.length; i++) {
+      customerCenter.push(
+        getRandomNumber(11, 38)
+      )
+      console.log(globalTime[i] + ' - ' + customerCenter[i]);
+    }
+  }
 };
 
 var capitolHill = {
-  minimumCustomers: 0,
-  maximumCustomers: 0,
-  averageCookies: 0,
-  randomNum: distributeRandomNumber
+  minimumCustomers: 20,
+  maximumCustomers: 38,
+  averageCookies: 2.3,
+  randomNum: function() {
+    console.log('Capitol Hill')
+    for (var i = 0; i < globalTime.length; i++) {
+      customerHill.push(
+        getRandomNumber(20, 38)
+      )
+      console.log(globalTime[i] + ' - ' + customerHill[i]);
+    }
+  }
 };
 
 var alki = {
-  minimumCustomers: 0,
-  maximumCustomers: 0,
-  averageCookies: 0,
-  randomNum: distributeRandomNumber
-};
-
-function distributeRandomNumber() {
-    for (var i = 0; i < locationArray.length; i++) {
-        for(var k = 0; k < globalTime.length; k++) {
-            this.minimumCustomers = getRandomNumber(23, 65);
-            this.minimumCustomers = getRandomNumber(23, 65);
-            this.averageCookies = getRandomNumber(1, 12);
-            break;
-        }
+  minimumCustomers: 2,
+  maximumCustomers: 16,
+  averageCookies: 4.6,
+  randomNum: function() {
+    console.log('Alki')
+    for (var i = 0; i < globalTime.length; i++) {
+      customerAlki.push(
+        getRandomNumber(2, 16)
+      )
+      console.log(globalTime[i] + ' - ' + customerAlki[i]);
     }
+  }
 };
 
 function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
+  return Math.random() * (max - min) + min;
+}
+
+function arraySum(array, sum) {
+  for (var i = 0; i < array.length; i++) {
+  sum += array[i]
   }
+  return sum;
+};
+
+function productSum(sum, avgCookies, total) {
+  total = sum * avgCookies;
+  locationArray.push(total);
+}
+
+firstAndPike.randomNum();
+seatacAirport.randomNum();
+seattleCenter.randomNum();
+capitolHill.randomNum();
+alki.randomNum();
+
+pikeSum = arraySum(customerPike, pikeSum);
+airportSum = arraySum(customerAirport, airportSum);
+centerSum = arraySum(customerCenter, centerSum);
+hillSum = arraySum(customerHill, hillSum);
+alkiSum = arraySum(customerAlki, alkiSum);
+
+productSum(pikeSum, pikeCookies, pikeTotal);
+productSum(airportSum, airportCookies, airportTotal);
+productSum(centerSum, centerCookies, centerTotal);
+productSum(hillSum, hillCookies, hillTotal);
+productSum(alkiSum, alkiCookies, alkiTotal);
+
+console.log(locationArray);
