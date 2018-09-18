39fe4ce216c224394b232b63d99b5d28b0ddea14
tyler <tyler.r.hood@gmail.com>
2018-09-18 13:47:25 -0700

    added a contructor function

--- js/app.js
+++ js/app.js
@@ -3,6 +3,15 @@
 
 var globalTime = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
+var Stores = function(name, minCust, maxCust, avgSale) {
+  this.storeName = name;
+  this.minCustomers = minCust;
+  this.maxCustomers = maxCust;
+  this.avgSale = avgSale;
+  this.avgCustomersHour = [];
+  this.avgCookiesHour = [];
+};
+
 var firstStore = {
   storeName: 'First and Pike',
   minCustomers: 23,
