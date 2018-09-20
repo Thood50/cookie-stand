86a9ba32a69ef110ed13b338f5468dcd82bb4cf0
tyler <tyler.r.hood@gmail.com>
2018-09-19 18:34:21 -0700

    fixed js....my elements are still building to many tbody's but ill work on that next

--- js/app.js
+++ js/app.js
@@ -44,23 +44,24 @@ Stores.prototype.getArraySum = function() {
 },
 
 Stores.prototype.render = function () {
-  var tbody = document.getElementsByTagName('tbody');
+  var theTable = document.getElementById('storeTable');
+  var tbody = document.createElement('tbody');
   var tr = document.createElement('tr');
   var th = document.createElement('th');
-  
   var storeName = document.createTextNode(this.storeName);
 
   th.appendChild(storeName);
   tr.appendChild(th);
-  tbody[0].appendChild(tr);
+  tbody.appendChild(tr);
+  theTable.appendChild(tbody);
 
   for (var i = 0; i < globalTime.length; i++) {
     var td = document.createElement('td');
     var cookieSales = document.createTextNode(this.avgCookiesHour[i]);
     td.appendChild(cookieSales);
     tr.appendChild(td);
-  } 
-  var td = document.createElement('td');
+  }
+  td = document.createElement('td');
   var totalCookies = document.createTextNode(this.totalCookies);
   td.appendChild(totalCookies);
   tr.appendChild(td);
@@ -77,16 +78,18 @@ Stores.prototype.getTotalHourlyCookies = function () {
 };
 
 function renderHeader() {
-  var thead = document.getElementsByTagName('thead');
+  var theTable = document.getElementById('storeTable');
+  var thead = document.createElement('thead');
   var tr = document.createElement('tr');
   var th = document.createElement('th');
 
   tr.appendChild(th);
-  thead[0].appendChild(tr);
+  thead.appendChild(tr);
 
   var storeLocations = document.createTextNode('Locations');
   th.appendChild(storeLocations);
   tr.appendChild(th);
+  theTable.appendChild(thead);
 
   for (var i = 0; i < globalTime.length; i++) {
     th = document.createElement('th');
@@ -102,16 +105,18 @@ function renderHeader() {
 }
 
 function renderFooter() {
-  var tfoot = document.getElementsByTagName('tfoot');
+  var theTable = document.getElementById('storeTable');
+  var tfoot = document.createElement('tfoot');
   var tr = document.createElement('tr');
   var th = document.createElement('th');
 
   tr.appendChild(th);
-  tfoot[0].appendChild(tr);
+  tfoot.appendChild(tr);
 
   var hourlyTotals = document.createTextNode('Hourly Totals');
   th.appendChild(hourlyTotals);
   tr.appendChild(th);
+  theTable.appendChild(tfoot);
 
   for(var i = 0; i < globalTime.length; i++) {
     th = document.createElement('th');
@@ -156,11 +161,11 @@ function collectData() {
   }
 }
 
-var firstStore = new Stores('First and Pike', 23, 65, 6.3);
-var secondStore = new Stores('SeaTac Airport', 3, 24, 1.2);
-var thirdStore = new Stores('Seattle Center', 11, 38, 3.7);
-var fourthStore = new Stores('Capitol Hill', 20, 38, 2.3);
-var fifthStore = new Stores('Alki', 2, 16, 4.6);
+new Stores('First and Pike', 23, 65, 6.3);
+new Stores('SeaTac Airport', 3, 24, 1.2);
+new Stores('Seattle Center', 11, 38, 3.7);
+new Stores('Capitol Hill', 20, 38, 2.3);
+new Stores('Alki', 2, 16, 4.6);
 
 newForm.addEventListener('submit', addNewStore);
 
